4f2d8cf0fc21b7134e7fcbfd775298ecd708b26c
charphi <charphi@users.noreply.github.com>
2025-02-03 11:16:38 +0000

    deploy: 04a7428830586ebb0b2350393947ac0892d56656

---
+++ docs/v2.5.1/apidocs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"nbbrd.picocsv"}];updateSearchResults();
---
+++ docs/v2.5.1/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"nbbrd.picocsv","l":"nbbrd.picocsv"}];updateSearchResults();
